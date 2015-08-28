b953cafab69877c53e06d01ab3c17b090afc37ea
Stefan Oslowski <sosl@users.noreply.github.com>
2015-08-28 11:54:45 +0200

    Provide subband offset as an argument

--- recording_system/recording-machine/PSR_8bit_Scripts/RunLuMP_universal.sh
+++ recording_system/recording-machine/PSR_8bit_Scripts/RunLuMP_universal.sh
@@ -6,16 +6,29 @@
 # Based on guidelines by James Anderson from MPIFR's LOFAR deki.
 #
 
-if [[ $# -ne 7 ]]
+set -o nounset
+
+if [[ $# -ne 8 ]]
 then
-    echo usage: $0 PSR Tint datadir starttime station lane verbosity_level
+    echo usage: $0 PSR Tint datadir starttime station lane subband_offset verbosity_level
     echo Tint is in minutes
+    echo subband_offset should match the setup of the beam, see observe-psr-universal.sh on the LCU
+    echo for 4 lanes it should be 12 and for 3 lanes set it to 93
     exit
 fi
 
-verbose=$7
+verbose=$8
 
 PSR=$1
+
+# pulsars for which high resolution data need to be produced to study scintillation:
+if [[ "$PSR" == "J0837+0610" ]] 
+then
+	LuMP_version=0
+else
+	LuMP_version=1
+fi
+
 
 [[ $verbose -gt 0 ]] && echo "Will observe Pulsar " $PSR
 
@@ -49,8 +62,9 @@ fi
 # set up auxiliary per-lane information
 beamlet_start=$(( ($lane-1)*122 ))
 beamlet_end=$(( ($lane)*122 ))
-subband_start=$(( ($lane-1)*122 + 12 ))
-subband_end=$(( ($lane)*122 + 12 ))
+subband_offset=$7
+subband_start=$(( ($lane-1)*122 + ${subband_offset} ))
+subband_end=$(( ($lane)*122 + ${subband_offset} ))
 [[ $verbose -gt 0 ]] && echo determined beamlet_start _end and subband_start _end: $beamlet_start $beamlet_end $subband_start $subband_end
 
 port=$(( 10000 + ${station_no}*10 + $lane ))
@@ -70,6 +84,6 @@ cd /media/scratch/observer/LuMP_${station}/
 mkdir -p $PSR
 cd $PSR
   
-    Basic_LuMP_Recorder.py --port=${port} --clock_speed=200 --beamlets_per_lane=122 --datadir=${datadir} --data_type_in=L_intComplex16_t --station_name=${station} --writer_type=LuMP1 --physical_beamlet_array='['${beamlet_start}':'${beamlet_end}']' --rcumode_array='[5]*122' --epoch_array='[J2000]*122' --verbose --duration=${TINT} --subband_array='['${subband_start}':'${subband_end}']' --filename_base=${PSR}_${lane} --sourcename_array="[$PSR]*122" --rightascension_array="[$RAJR]*122" --declination_array="[$DECJR]*122" --start_date=${starttime} --recorder_num_cores=2
+    Basic_LuMP_Recorder.py --port=${port} --clock_speed=200 --beamlets_per_lane=122 --datadir=${datadir} --data_type_in=L_intComplex16_t --station_name=${station} --writer_type=LuMP${LuMP_version} --physical_beamlet_array='['${beamlet_start}':'${beamlet_end}']' --rcumode_array='[5]*122' --epoch_array='[J2000]*122' --verbose --duration=${TINT} --subband_array='['${subband_start}':'${subband_end}']' --filename_base=${PSR}_${lane} --sourcename_array="[$PSR]*122" --rightascension_array="[$RAJR]*122" --declination_array="[$DECJR]*122" --start_date=${starttime} --recorder_num_cores=2
 
 [[ $verbose -gt 0 ]] && echo "Finished observation."
