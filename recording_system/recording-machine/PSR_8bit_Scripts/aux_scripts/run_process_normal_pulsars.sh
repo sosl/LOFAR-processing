a436485707341b40464124e1f84d6b88f6bb6f1c
Stefan Oslowski <sosl@users.noreply.github.com>
2016-03-05 09:55:31 +0100

    Match run_normal usage to the cleaned-up version

--- recording_system/recording-machine/PSR_8bit_Scripts/aux_scripts/run_process_normal_pulsars.sh
+++ recording_system/recording-machine/PSR_8bit_Scripts/aux_scripts/run_process_normal_pulsars.sh
@@ -4,6 +4,6 @@ station=$1
 
 while [ 0 -lt 1 ]
 do
-   ~/PSR_8bit_Scripts/Reduce_LuMP_dual_lane_3lanes.sh 5 $station
+   ~/PSR_8bit_Scripts/Reduce_LuMP_dual_lane_3lanes.sh $station
    sleep 5m
 done
